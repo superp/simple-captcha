a2789153422c672faf15841abb20283d423135c2
Nick Hengeveld <nickh@verticalresponse.coM>
2010-10-21 17:55:50 -0700

    Captcha images are small, try send_data instead of send_file + X-Accel-Redirect

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -5,10 +5,9 @@ class SimpleCaptchaController < ActionController::Metal
   # GET /simple_captcha
   def show
     unless params[:id].blank?
-      image_file = generate_simple_captcha_image(params[:id])
-      response.headers['X-Accel-Redirect'] = image_file
-      send_file(
-        image_file,
+      image = File.read generate_simple_captcha_image(params[:id])
+      send_data(
+        image,
         :type => 'image/jpeg',
         :disposition => 'inline',
         :filename => 'simple_captcha.jpg')
